1a0be56508e1cd10aed9820383a0c48636a604ee
olli0601 <oliver.ratmann@imperial.ac.uk>
2025-05-21 10:41:26 +0100

    removed rstan part in intermediate

--- Intermediate/Bernoulli/Intermediate_Bernoulli.R
+++ Intermediate/Bernoulli/Intermediate_Bernoulli.R
@@ -1,9 +1,12 @@
-setwd("/Users/afrenisterrantino/Library/CloudStorage/OneDrive-TheAlanTuringInstitute/ImperialCollege/STAN/YS_Example/Intermediate/Bernoulli/")
+setwd("/Users/or105/git/Implementing-new-Stan-Distribution-Tutorial/Intermediate/Bernoulli")
+
 ##############################
 # Chunk code: set library
 #############################
+#############################
 library(cmdstanr)
-set_cmdstan_path("/Users/afrenisterrantino/.cmdstan/cmdstan-2.36.0")
+
+#set_cmdstan_path("/Users/afrenisterrantino/.cmdstan/cmdstan-2.36.0")
 
 ##############################
 # Chunk code:  model 
@@ -11,17 +14,17 @@ set_cmdstan_path("/Users/afrenisterrantino/.cmdstan/cmdstan-2.36.0")
 
 
 mod <- cmdstan_model('bernoulli_example.stan',
-    include_paths=getwd(),
-    cpp_options=list(USER_HEADER='external.hpp'),
-    stanc_options = list("allow-undefined")
-)
+                     include_paths = getwd(),
+                     cpp_options = list(USER_HEADER = 'external.hpp'),
+                     stanc_options = list("allow-undefined")
+                     )
 
 ##############################
 # Chunk code:  model 
 #############################
 
 
-fit <- mod$sample(data=list(N = 10, y = c(0,1,0,0,0,0,1,1,0,1)))
+fit <- mod$sample(data = list(N = 10, y = c(0,1,0,0,0,0,1,1,0,1)))
 fit$draws('odds')
 
 ##############################
@@ -53,32 +56,4 @@ fit$draws('odds')
 ##############################
 # Chunk code:  rm 
 #############################
-rm(list=ls())
-
-##############################
-# Chunk code:  With rstan
-#############################
-library(rstan)
-library(ggplot2)
-library(gridExtra)
-library(bayesplot)
-##############################
-# Chunk code:  rstan 
-#############################
-model <- stan_model('bernoulli_example.stan',
-    allow_undefined = TRUE,
-    includes = paste0('\n#include "', file.path(getwd(), 'external_rstan.hpp'), '"\n'),
-)
-
-fit <- sampling(model, data = list(N = 10, y = c(0,1,0,0,0,0,0,0,0,1)))
-extract(fit)$odds
-
-monitor(fit)
-
-draws <- as.data.frame(fit)
-mcmc_hist(draws, pars='theta', bins=30)
-mcmc_hist(draws, pars = 'odds',bins=50)
-
-
-
-
+rm(list = ls())
